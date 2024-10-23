d8610bf9c24a99bdfab212ba607638cf647ffc91
asmabenbali <assmabenbali5@gmail.com>
2024-10-23 23:15:53 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Crud from './Component/Crud';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Crud />
   </React.StrictMode>
 );
 
